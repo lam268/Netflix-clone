3e092e8202ce165481e0c186c0b573e672a13931
lam268 <lamlevu26@gmail.com>
2020-12-24 13:55:42 +0700

    change jade to pug

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true,
